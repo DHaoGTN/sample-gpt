cedcd3af176321e5e4fe9f02c5b32205d2e43141
DHaoGTN <d.hao@gtn-vietnam.com>
2023-09-12 15:31:55 +0700

    Change to use other package OpenAI API for php7.4

--- src/app/Http/Services/OpenAIService.php
+++ src/app/Http/Services/OpenAIService.php
@@ -2,22 +2,55 @@
 
 namespace App\Http\Services;
 
-use OpenAI\Laravel\Facades\OpenAI;
+// use OpenAI\Laravel\Facades\OpenAI;
 use Illuminate\Support\Arr;
+use Orhanerday\OpenAi\OpenAi;
 
 class OpenAIService
 {
 
+    /**
+     * Use openai-php/laravel, require php8.1
+     */
+    // public function callAPI($prompt)
+    // {
+    //     $messages = [
+    //         ['role' => 'user', 'content' => $prompt],
+    //     ];
+    //     $response = OpenAI::chat()->create([
+    //         'model' => 'gpt-3.5-turbo',
+    //         'messages' => $messages,
+    //     ]);
+    //     return Arr::get($response, 'choices.0.message')['content'];
+    // }
+
+    /**
+     * Use orhanerday/open-ai, require php7.4
+     */
     public function callAPI($prompt)
     {
-        $messages = [
-            ['role' => 'user', 'content' => $prompt],
-        ];
-        $response = OpenAI::chat()->create([
+        $open_ai = new OpenAi(env('OPENAI_API_KEY'));
+
+        $chat = $open_ai->chat([
             'model' => 'gpt-3.5-turbo',
-            'messages' => $messages,
+            'messages' => [
+                [
+                    "role" => "assistant",
+                    "content" => $prompt
+                ]
+            ],
+            'temperature' => 1.0,
+            'max_tokens' => 4000,
+            'frequency_penalty' => 0,
+            'presence_penalty' => 0,
         ]);
-        return Arr::get($response, 'choices.0.message')['content'];
+
+        // dd($chat);
+        // decode response
+        $d = json_decode($chat);
+        // Get Content
+        $res = $d->choices[0]->message->content;
+        return $res;
     }
 
 }
